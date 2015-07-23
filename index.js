cf401cac098d9765c1ef669b205a90209ed0b0fe
Mihai Ene <dkrandu@yahoo.com>
2015-07-23 02:00:17 +0100

    General layout with channels display.

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 'use strict';
 
 var token = require('./lib/token.js');
-var Api = require('./lib/api.js');
-//var Window = require('./lib/window.js');
-
-var api = new Api(token);
-api.on('ready', function() {
-    // var window = new Window({
-    //     self: api.self
-    // });
-});
+var Precis = require('./lib/precis');
+new Precis(token);
 
